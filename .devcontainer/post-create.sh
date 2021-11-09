76fe497dfb21e41ec4b810129a3adb3413265865
Z. Patrick Lewis <patrick_lewis@unc.edu>
2021-11-08 23:23:18 -0500

    load jekyll_github_token env var

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # https://github.com/olivergondza/bash-strict-mode
-set -euo pipefail
-trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+# Exit when a command fails
+set -o errexit
+# Exit when any command in a pipeline returns a failure code
+set -eu pipefail
+# Exit when the script tries to use undeclared variables
+set -o nounset
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Get the folder that is the root of this repo
-folder="$(dirname "${DIR}")"
+folder="${DIR}"
+
+if [[ "${folder}" == *"/.devcontainer" ]]; then
+    folder="$(dirname "${DIR}")"
+fi
+
 if [ -d "${folder}/docs" ]; then
     folder="${folder}/docs"
 fi
@@ -15,7 +25,8 @@ echo "folder: ${folder}"
 
 # Install the version of Bundler.
 if [ -f "${folder}/Gemfile.lock" ] && grep "BUNDLED WITH" "${folder}/Gemfile.lock" > /dev/null; then
-    cat "${folder}/Gemfile.lock" | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+    version=$(cat "${folder}/Gemfile.lock" | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1)
+    gem install bundler -v "${version}"
 fi
 
 # If there's a Gemfile, then run `bundle install`
@@ -27,5 +38,5 @@ fi
 # Attempt to serve the Jekyll site with livereloading
 if command -V "bundle" &> /dev/null; then
     cd "${folder}"
-    bundle exec jekyll serve --livereload --incremental
+    bundle exec jekyll serve --watch
 fi
