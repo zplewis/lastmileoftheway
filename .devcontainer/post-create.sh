1e4f55f589eb49d98259c2a4de64faf16d0972e6
Z. Patrick Lewis <patrick_lewis@unc.edu>
2021-11-26 14:07:54 -0500

    add comparison page as index.html

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -38,7 +38,8 @@ fi
 # Attempt to serve the Jekyll site with livereloading
 if command -V "bundle" &> /dev/null; then
     cd "${folder}"
-    bundle exec jekyll serve --watch --incremental
+    # bundle exec jekyll serve --watch --incremental
+    bundle exec jekyll serve --watch
 fi
 
 if [ -f "${folder}/package.json" ]; then
