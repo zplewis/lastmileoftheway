144e18730a6a5ceae19f66e54a831e37a36f9104
Z. Patrick Lewis <patrick_lewis@unc.edu>
2022-02-10 01:02:16 -0500

    Add toggle for including a viewing before service

--- resources/views/guide/demographics/dates.blade.php
+++ resources/views/guide/demographics/dates.blade.php
@@ -11,7 +11,15 @@
 </div>
 
 <div class="col-12">
-    @include('guide.field', ['id' => 'dateService', 'inputType' => 'date', 'labelText' => 'Service date (if known)'])
+    @include('guide.field', ['id' => 'dateService', 'inputType' => 'date', 'labelText' => 'Desired Service date'])
+</div>
+
+<div class="col-12">
+    <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+        <label class="form-check-label" for="flexSwitchCheckChecked">Include a
+            <a href="/resources/glossary#viewing" title="Glossary - Viewing">viewing</a> one hour prior to start of the service</label>
+    </div>
 </div>
 
 @endsection
