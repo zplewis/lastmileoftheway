278d354f1f1aa1a2e2a6f7c581ee755a7ee0e908
Z. Patrick Lewis <patrick_lewis@unc.edu>
2022-03-15 18:00:11 -0400

    removed duplicate include switches

--- resources/views/guide/customize-service/recessional.blade.php
+++ resources/views/guide/customize-service/recessional.blade.php
@@ -2,11 +2,4 @@
 
 @section('guide.content')
 
-<div class="col-12">
-    <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-        <label class="form-check-label" for="flexSwitchCheckChecked">Include a {{ $pageDesc }} in this service</label>
-    </div>
-</div>
-
 @endsection
